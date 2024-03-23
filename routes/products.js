f07e92cf4b155b805c604baef27a882bd5412e54
Aliff Ruslan <nav_spect@yahoo.com>
2024-03-23 03:27:20 +0000

    update routes

--- routes/products.js
+++ routes/products.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const productDAL = require('../dal/productDAL');
+const product = require('../dal/product');
 const { createProductForm, bootstrapField } = require('../forms');
 
 const router = express.Router();
 
 router.get('/', async function (req, res) {
-    const products = await productDAL.getAllProducts();
+    const products = await product.getAllProducts();
     res.render("products/index", {
         products: products.toJSON()
     });
 });
 
 router.get('/create', async function (req, res) {
-    const allCategories = await productDAL.getAllCategories();
-    const allTags = await productDAL.getAllTags();
+    const allCategories = await product.getAllCategories();
+    const allTags = await product.getAllTags();
     const productForm = createProductForm(allCategories, allTags);
     res.render('products/create', {
         form: productForm.toHTML(bootstrapField)
@@ -21,15 +21,15 @@ router.get('/create', async function (req, res) {
 });
 
 router.post('/create', async function (req, res) {
-    const product = await productDAL.createProduct(req.body);
+    const product = await product.createProduct(req.body);
     req.flash("success_messages", "New product has been added");
     res.redirect('/products');
 });
 
 router.get('/:product_id/update', async function (req, res) {
-    const product = await productDAL.getProductById(req.params.product_id);
-    const allCategories = await productDAL.getAllCategories();
-    const allTags = await productDAL.getAllTags();
+    const product = await product.getProductById(req.params.product_id);
+    const allCategories = await product.getAllCategories();
+    const allTags = await product.getAllTags();
     const productForm = createProductForm(allCategories, allTags);
     res.render('products/update', {
         form: productForm.toHTML(bootstrapField),
@@ -38,20 +38,20 @@ router.get('/:product_id/update', async function (req, res) {
 });
 
 router.post('/:product_id/update', async function (req, res) {
-    await productDAL.updateProduct(req.params.product_id, req.body);
+    await product.updateProduct(req.params.product_id, req.body);
     req.flash("success_messages", "Product has been updated");
     res.redirect('/products');
 });
 
 router.get('/:product_id/delete', async function (req, res) {
-    const product = await productDAL.getProductById(req.params.product_id);
+    const product = await product.getProductById(req.params.product_id);
     res.render('products/delete', {
         product: product.toJSON()
     });
 });
 
 router.post('/:product_id/delete', async function (req, res) {
-    await productDAL.deleteProduct(req.params.product_id);
+    await product.deleteProduct(req.params.product_id);
     req.flash("success_messages", "Product has been deleted");
     res.redirect('/products');
 });
